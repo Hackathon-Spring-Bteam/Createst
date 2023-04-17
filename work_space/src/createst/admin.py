971b2606d72d711373116ae78a3272d796e91551
Hiroki Maeda <maetyanidattyo169@gmail.com>
2023-04-17 22:22:22 +0900

    models.py作成

--- work_space/src/createst/admin.py
+++ work_space/src/createst/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Label, User, Test, Problem, Choice
 
-# Register your models here.
+admin.site.register(Label)
+admin.site.register(User)
+admin.site.register(Test)
+admin.site.register(Problem)
+admin.site.register(Choice)
