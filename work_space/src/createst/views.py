85fa348d7c964f1fb565047cb2ec4f47ededdc4d
hitugihane <hituzihane01@gmail.com>
2023-05-25 15:18:31 +0900

    小修整

--- work_space/src/createst/views.py
+++ work_space/src/createst/views.py
@@ -12,7 +12,7 @@ from django.views.generic import TemplateView
 import json
 from django.contrib import messages
 import time
-
+from django.views.generic import ListView
 # Index.html
 class IndexView(LoginRequiredMixin, View):
     def get(self, request):
